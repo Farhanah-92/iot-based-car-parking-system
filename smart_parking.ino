d8c533bbe9626b24ad290d77edcf502ad37c2c86
Farhanah-92 <farhanahfarveen@gmail.com>
2026-07-31 22:33:41 +0530

    Add smart parking source code

--- smart_parking.ino
+++ smart_parking.ino
@@ -4,14 +4,9 @@
 
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
-#include <Wire.h>
-#include <LiquidCrystal_I2C.h>
 
-// WiFi credentials
 char ssid[] = "YOUR_WIFI_NAME";
 char pass[] = "YOUR_WIFI_PASSWORD";
-
-LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 // IR sensor pins
 #define IR1 D1
@@ -27,12 +22,7 @@ void setup() {
   pinMode(IR3, INPUT);
   pinMode(IR4, INPUT);
 
-  lcd.begin();
-  lcd.backlight();
-  lcd.setCursor(0, 0);
-  lcd.print("Car Parking IoT");
-  delay(2000);
-  lcd.clear();
+  Serial.println("Car Parking IoT System");
 
   Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
 }
@@ -45,18 +35,16 @@ void loop() {
   int s3 = digitalRead(IR3);
   int s4 = digitalRead(IR4);
 
-  lcd.setCursor(0, 0);
-  lcd.print("S1:");
-  lcd.print(s1 == LOW ? "Occ" : "Emp ");
-  lcd.print(" S2:");
-  lcd.print(s2 == LOW ? "Occ" : "Emp");
+  Serial.print("S1: ");
+  Serial.print(s1 == LOW ? "Occupied" : "Empty");
+  Serial.print(" | S2: ");
+  Serial.print(s2 == LOW ? "Occupied" : "Empty");
+  Serial.print(" | S3: ");
+  Serial.print(s3 == LOW ? "Occupied" : "Empty");
+  Serial.print(" | S4: ");
+  Serial.println(s4 == LOW ? "Occupied" : "Empty");
 
-  lcd.setCursor(0, 1);
-  lcd.print("S3:");
-  lcd.print(s3 == LOW ? "Occ" : "Emp ");
-  lcd.print(" S4:");
-  lcd.print(s4 == LOW ? "Occ" : "Emp");
-
+  // Send status to Blynk
   Blynk.virtualWrite(V0, s1 == LOW ? 255 : 0);
   Blynk.virtualWrite(V1, s2 == LOW ? 255 : 0);
   Blynk.virtualWrite(V2, s3 == LOW ? 255 : 0);
